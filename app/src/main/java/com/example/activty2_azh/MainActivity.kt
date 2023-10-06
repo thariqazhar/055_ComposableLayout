c255f4bc4361d7e90157153ebc0bf466c834222c
Prima06112002 <ramadhan06112002@gmail.com>
2023-10-06 18:45:51 +0700

    function TampilText

--- app/src/main/java/com/example/activty2_azh/MainActivity.kt
+++ app/src/main/java/com/example/activty2_azh/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.activty2_azh
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.activty2_azh.ui.theme.Activty2_azhTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,12 +28,33 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                    color = MaterialTheme.colorScheme.background)
+                {
                     Greeting("Android")
                 }
             }
         }
+    }
+}
+
+@Composable
+fun TampilText(message:String, from:String, modifier: Modifier=Modifier){
+    Column( verticalArrangement = Arrangement.Center, modifier =  modifier){
+        Text(
+            color = Color.Blue,
+            text = message,
+            fontSize = 80.sp,
+            lineHeight = 110.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            color = Color.Red,
+            text = from,
+            fontSize = 60.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
     }
 }
 
